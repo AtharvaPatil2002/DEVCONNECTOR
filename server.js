c54ea6a844c1476b472a8de06065c2f093521826
AtharvaPatil2002 <patilatharva876@gmail.com>
2022-01-31 21:15:31 +0700

    Message

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const express= require('express');
+const express = require('express');
 const app = express();
-app.get('/',(req,res)=> res.send('API RUNNING'))
-const PORT = process.env.PORT|| 5000;
+const connectDB = require('./config/db.js');
+connectDB();
+//INIT
+app.use(express.json({extended : false}));
+app.get('/', (req, res) => res.send('API RUNNING'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
